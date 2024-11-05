6fc5ffdf3b48e9b1c4eb47158210532397903c4b
harrisonarth <arth.harrison@gmail.com>
2024-11-05 16:51:22 -0600

    remove unwanted tools

--- kubejs/server_scripts/recipechanges.js
+++ kubejs/server_scripts/recipechanges.js
@@ -11,4 +11,26 @@ ServerEvents.recipes(event => {
       .shapeless(recipe.output, recipe.input)
       .damageIngredient("#minecraft:hoes");
   });
+
+  const materials = [
+    'bone', 'coal', 'ender', 'fiery', 'glowstone', 'lapis', 
+    'nether', 'paper', 'prismarine', 'quartz', 'redstone', 
+    'slime',
+  ];
+
+  // List of tool types by mod
+  const toolsByMod = {
+    'vanillaaiots': ['aiot'],
+    'simplytools': ['hammer', 'excavator', 'hammer_head', 'excavator_head',],
+    'morevanillatools': ['sword', 'shovel', 'axe', 'pickaxe', 'hoe',],
+  };
+      // Generate and hide items dynamically
+  Object.entries(toolsByMod).forEach(([mod, tools]) => {
+        materials.forEach(material => {
+            tools.forEach(tool => {
+                const itemID = `${mod}:${material}_${tool}`;
+                event.remove({ output: itemID });
+            });
+        });
+    });
 });
